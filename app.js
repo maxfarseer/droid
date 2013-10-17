7a9854ca8b7b1c6270b8455150348785758a81a6
Max P <maxfarseer@gmail.com>
2013-10-17 17:58:42 +0400

    bootsrap, jquery added

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+//app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+require('./routes')(app);
+
+//app.get('/', routes.index);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
